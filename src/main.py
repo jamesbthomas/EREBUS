4851e53ecbf15cc56ea8f92b2bfc8d691d95cef7
jamesbthomas <james.b.thomas@mac.com>
2016-11-27 18:32:24 -0500

    added initial REBUS home page

--- src/main.py
+++ src/main.py
@@ -7,21 +7,18 @@
 
 #imports
 from Tkinter import *
+import tkFont
 from ceasar import *
 import tkMessageBox
 
-#global variables to represent the current window
-scrambler = False
-saver = False
-retriever = False
-
 def ceasar():
-  scramText = doCeasar(unscram.get(),shift.get())
-  scram.set(scramText)
+  if shift.get():
+    scramText = doCeasar(unscram.get(),shift.get())
+    scram.set(scramText)
 
-def doNothing():
-    global scrambler
-    if not scrambler:
+def ceasarMenu():
+    global ceasar
+    if not ceasar:
         unscramframe = Frame(window)
         unscramframe.pack( side = TOP )
         shiftframe = Frame(window)
@@ -29,15 +26,15 @@ def doNothing():
         butframe = Frame(window)
         butframe.pack()
         botframe = Frame(window)
-        botframe.pack( side = BOTTOM )
+        botframe.pack()
         
-        unscramLabel = Label(unscramframe,text="Enter Unencrypted Password: ")
+        unscramLabel = Label(unscramframe,text="Enter Phrase: ")
         unscramLabel.pack(side = LEFT) 
         global unscram
         unscram = StringVar()
         unscramEntry = Entry(unscramframe,bd = 5,textvariable = unscram)
         unscramEntry.pack(side = LEFT)
-        shiftLabel = Label(shiftframe,text="Enter Shift Amount: ")
+        shiftLabel = Label(shiftframe,text="Enter Shift: ")
         shiftLabel.pack(side = LEFT)
         global shift
         shift = StringVar()
@@ -47,23 +44,51 @@ def doNothing():
         scramButton = Button(butframe,text = "Encrypt!", command = ceasar)
         scramButton.pack()
         
-        scramOutLabel = Label(botframe,text="Encrypted Password: ")
+        scramOutLabel = Label(botframe,text="Password: ")
         scramOutLabel.pack(side=LEFT)
         global scram
         scram = StringVar()
         scramOut = Entry(botframe,bd = 5,textvariable = scram)
         scramOut.pack(side=LEFT)
         
-        scrambler = True
+        ceasar = True
+
+def doHome():
+    global home
+    if not home:
+        homeFrame = Frame(window)
+        homeFrame.pack()
+        
+        titleLabel = Label(homeFrame,text="EREBUS",font=titleFont)
+        titleLabel.pack()
+        
+        subtitleLabel = Label(homeFrame,text="The Home Password Vault",font=subTitleFont)
+        subtitleLabel.pack()
+    
+        home = True
 
 if __name__ == '__main__':
+    #make some variables
+    ceasar = False
+    home = False
+    #root window
     window = Tk()
-    frame = Frame(window)
-    frame.pack()
+    window.minsize(width=250,height=150)
+    #make some fonts
+    titleFont = tkFont.Font(family="Arial Black",size=20)
+    subTitleFont = tkFont.Font(family="Helvetica",size=12)
+    doHome()
     #menubar
     bar = Menu(window)
+    #home menu
+    bar.add_command(label="Home",command=doHome)
     #scrambler menu option
-    bar.add_command(label="Password Creator",command=doNothing)
-    
+    scrambler = Menu(bar,tearoff=0)
+    scrambler.add_command(label="Caesar",command=ceasarMenu)
+    bar.add_cascade(label="Creator",menu=scrambler)
+    #saver menu option
+    bar.add_command(label="Saver")
+    #retriever menu option
+    bar.add_command(label="Retriever")
     window.config(menu=bar)
     window.mainloop()
